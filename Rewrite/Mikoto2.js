681b9539390e82b88b94145fce96034edd0f812d
TS <105903283+crctdd@users.noreply.github.com>
2025-03-11 17:49:01 +0800

    Delete Rewrite/Mikoto2.js

--- Rewrite/Mikoto2.js
+++
@@ -1,5 +0,0 @@
-[rewrite_local]
-^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/crctdd/QuantumultX/refs/heads/main/Rewrite/Mikoto1.js
-
-[mitm] 
-hostname = api.dragonlongzhu.cn
